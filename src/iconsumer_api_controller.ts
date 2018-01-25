2b7e090c68ecd0f09c0b2881d20bda9322f555f8
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-25 10:59:00 +0100

    :bug: make the controller handle the response, not the router

--- src/iconsumer_api_controller.ts
+++ src/iconsumer_api_controller.ts
@@ -2,17 +2,17 @@ import {Request, Response} from 'express';
 import {IEventList, IProcessModel, IProcessModelList, IUserTaskList} from './index';
 
 export interface IConsumerApiController {
-  getProcessModels(request: Request, response: Response): Promise<IProcessModelList>;
-  getProcessModelByKey(request: Request, response: Response): Promise<IProcessModel>;
+  getProcessModels(request: Request, response: Response): Promise<void>;
+  getProcessModelByKey(request: Request, response: Response): Promise<void>;
   startProcess(request: Request, response: Response): Promise<void>;
 
-  getEventsByProcessModel(request: Request, response: Response): Promise<IEventList>;
-  getEventsByCorrelation(request: Request, response: Response): Promise<IEventList>;
-  getEventsByVerifiedCorrelation(request: Request, response: Response): Promise<IEventList>;
+  getEventsByProcessModel(request: Request, response: Response): Promise<void>;
+  getEventsByCorrelation(request: Request, response: Response): Promise<void>;
+  getEventsByVerifiedCorrelation(request: Request, response: Response): Promise<void>;
   triggerEvent(request: Request, response: Response): Promise<void>;
 
-  getUserTasksByProcessModel(request: Request, response: Response): Promise<IUserTaskList>;
-  getUserTasksByCorrelation(request: Request, response: Response): Promise<IUserTaskList>;
-  getUserTasksByVerifiedCorrelation(request: Request, response: Response): Promise<IUserTaskList>;
+  getUserTasksByProcessModel(request: Request, response: Response): Promise<void>;
+  getUserTasksByCorrelation(request: Request, response: Response): Promise<void>;
+  getUserTasksByVerifiedCorrelation(request: Request, response: Response): Promise<void>;
   finishUserTask(request: Request, response: Response): Promise<void>;
 }
