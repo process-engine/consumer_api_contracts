f2867d2bfe8953786a070da54bd5cc1d273d8218
ElRaptorus <christian.werner@5minds.de>
2018-02-15 11:24:51 +0100

    Group exported notification channels and routes

--- src/constants.ts
+++ src/constants.ts
@@ -1,38 +1,41 @@
-// process-model-routes
-export const processModelsRoute: string = '/process_models';
-export const processModelRoute: string = `/process_models/:process_model_key`;
-export const startProcessRoute: string = `${processModelRoute}/start_events/:start_event_key/trigger`;
+// Routes
+const processModelsRoute: string = '/process_models';
+const processModelRoute: string = '/process_models/:process_model_key';
+const correlationRoute: string = `/correlations/:correlation_id`;
+const eventsRoute: string = `/events`;
+const eventRoute: string = `/events/:event_id`;
+const userTasksRoute: string = `/user_tasks`;
+const userTaskRoute: string = `/user_tasks/:user_task_id`;
 
-export const Routes: { [routeName: string]: string; } = {
-  processModelRoute: '`/process_models/:process_model_key`',
+export const routes: { [routeName: string]: string; } = {
+  // process-model-routes
+  processModelsRoute: processModelsRoute,
+  processModelRoute: processModelRoute,
+  startProcessRoute: `${processModelRoute}/start_events/:start_event_key/trigger`,
+  // event-routes
+  eventsByProcessModelRoute: `${processModelRoute}${eventsRoute}`,
+  eventsByCorrelationRoute: `${correlationRoute}${eventsRoute}`,
+  eventsByVerifiedCorrelationRoute: `${processModelRoute}${correlationRoute}${eventsRoute}`,
+  triggerEventRoute: `${processModelRoute}${correlationRoute}${eventRoute}/trigger`,
+  // user-task-routes
+  userTasksByProcessModelRoute: `${processModelRoute}${userTasksRoute}`,
+  userTasksByCorrelationRoute: `${correlationRoute}${userTasksRoute}`,
+  userTasksByVerifiedCorrelationRoute: `${processModelRoute}${correlationRoute}${userTasksRoute}`,
+  finishUserTaskRoute: `${processModelRoute}${correlationRoute}${userTaskRoute}/finish`,
 };
 
-const correlationRoute: string = `/correlations/:correlation_id`;
+// Notification-channels
+const processModelKeyConstant: string = ':process_model_key';
+const correlationIdConstant: string = ':correlation_id';
+const elementKeyConstant: string = ':element_key';
+const processNotificationsChannel: string = '/process_notifications';
 
-// event-routes
-const eventsRoute: string = `/events`;
-export const eventsByProcessModelRoute: string = `${processModelRoute}${eventsRoute}`;
-export const eventsByCorrelationRoute: string = `${correlationRoute}${eventsRoute}`;
-export const eventsByVerifiedCorrelationRoute: string = `${processModelRoute}${correlationRoute}${eventsRoute}`;
-
-const eventRoute: string = `/events/:event_id`;
-export const triggerEventRoute: string = `${processModelRoute}${correlationRoute}${eventRoute}/trigger`;
-
-// user-task-routes
-const userTasksRoute: string = `/user_tasks`;
-export const userTasksByProcessModelRoute: string = `${processModelRoute}${userTasksRoute}`;
-export const userTasksByCorrelationRoute: string = `${correlationRoute}${userTasksRoute}`;
-export const userTasksByVerifiedCorrelationRoute: string = `${processModelRoute}${correlationRoute}${userTasksRoute}`;
-
-const userTaskRoute: string = `/user_tasks/:user_task_id`;
-export const finishUserTaskRoute: string = `${processModelRoute}${correlationRoute}${userTaskRoute}/finish`;
-
-// notification-channels
-export const processModelKeyConstant: string = ':process_model_key';
-export const correlationIdConstant: string = ':correlation_id';
-export const elementKeyConstant: string = ':element_key';
-
-export const processNotificationsChannel: string = '/process_notifications';
-export const processNotificationsByProcessModelChannel: string = `/process_models/${processModelKeyConstant}${processNotificationsChannel}`;
-export const processNotificationsByCorrelationChannel: string = `/correlation/${correlationIdConstant}${processNotificationsChannel}`;
-export const processNotificationByElementChannel: string = `/correlation/${correlationIdConstant}/elements/${elementKeyConstant}${processNotificationsChannel}`;
+export const notificationChannels: { [channelName: string]: string } = {
+  processModelKeyConstant: processModelKeyConstant,
+  correlationIdConstant: correlationIdConstant,
+  elementKeyConstant: elementKeyConstant,
+  processNotificationsChannel: processNotificationsChannel,
+  processNotificationsByProcessModelChannel: `/process_models/${processModelKeyConstant}${processNotificationsChannel}`,
+  processNotificationsByCorrelationChannel: `/correlation/${correlationIdConstant}${processNotificationsChannel}`,
+  processNotificationByElementChannel: `/correlation/${correlationIdConstant}/elements/${elementKeyConstant}${processNotificationsChannel}`,
+};
