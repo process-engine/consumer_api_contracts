cb09a9ab1df4072161929ede0397254a95caed3b
ElRaptorus <christian.werner@5minds.de>
2018-03-09 15:35:47 +0100

    Fix api routes

--- src/data_models/routes.ts
+++ src/data_models/routes.ts
@@ -21,20 +21,20 @@ export interface IRoutes {
 
 export const routes: IRoutes = {
   // Process models
-  processModels: 'process_models',
-  processModel: 'process_models/:process_model_key',
+  processModels: '/process_models',
+  processModel: '/process_models/:process_model_key',
   startProcess: '/process_models/:process_model_key/start_events/:start_event_key/start',
   startProcessAndAwaitEndEvent: '/process_models/:process_model_key/start_events/:start_event_key/end_event/:end_event_key/start_and_resolve_by_end_event',
   // Events
-  processModelEvents: 'process_models/:process_model_key/events',
-  correlationEvents: 'correlations/:correlation_id/events',
-  processModelCorrelationEvents: 'process_models/:process_model_key/correlations/:correlation_id/events',
+  processModelEvents: '/process_models/:process_model_key/events',
+  correlationEvents: '/correlations/:correlation_id/events',
+  processModelCorrelationEvents: '/process_models/:process_model_key/correlations/:correlation_id/events',
   triggerEvent: '/events/:event_id/trigger',
   triggerProcessModelCorrelationEvent: '/process_models/:process_model_key/correlations/:correlation_id/events/:event_id/trigger',
   // UserTasks
-  processModelUserTasks: 'process_models/:process_model_key/user_tasks',
-  correlationUserTasks: 'correlations/:correlation_id/user_tasks',
-  processModelCorrelationUserTasks: 'process_models/:process_model_key/correlations/:correlation_id/user_tasks',
+  processModelUserTasks: '/process_models/:process_model_key/user_tasks',
+  correlationUserTasks: '/correlations/:correlation_id/user_tasks',
+  processModelCorrelationUserTasks: '/process_models/:process_model_key/correlations/:correlation_id/user_tasks',
   finishUserTask: '/user_tasks/:user_task_id/finish',
   finishProcessModelCorrelationUserTask: '/process_models/:process_model_key/correlations/:correlation_id/user_tasks/:user_task_id/finish',
 };
