9d0402ab71b6c284838ca02101b99a9b276c4c37
Heiko Mathes <heiko.mathes@gmail.com>
2018-03-27 10:17:13 +0200

    :bulb: add todo-comment

--- src/messagebus_api/process_event_message.ts
+++ src/messagebus_api/process_event_message.ts
@@ -4,5 +4,5 @@ export class ProcessEventMessagePayload {
   public process_model_key: string;
   public event_key: string;
   public correlation_id: string;
-  public payload: any;
+  public payload: any; // TODO: Define ProcessEventMessagePayload-body
 }
