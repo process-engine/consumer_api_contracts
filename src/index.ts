95763ea94cc629d12cc2b9b6737636e25ad58197
Heiko Mathes <heiko.mathes@gmail.com>
2018-03-28 14:05:37 +0200

    :fire: remove controller and router interfaces. No one relies on them

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export * from './data_models/index';
 export * from './messagebus_api/index';
 
-export * from './iconsumer_api_controller';
-export * from './iconsumer_api_router';
 export * from './iconsumer_api_service';
 export * from './consumer_context';
 export * from './consumer_request';
