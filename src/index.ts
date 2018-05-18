d8de4e37f0de6c8e732b10296971d3a7b8bac9d6
ElRaptorus <christian.werner@5minds.de>
2018-05-18 11:10:00 +0200

    Move rest settings to top level folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+export * from './correlation/index';
 export * from './data_models/index';
 export * from './messagebus_api/index';
 
-export * from './iconsumer_api_service';
 export * from './consumer_context';
 export * from './consumer_request';
+export * from './iconsumer_api_service';
+export * from './rest_settings';
