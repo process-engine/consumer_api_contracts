5d50c057b0b61e926e15ea6663a02034dd7741d7
ElRaptorus <christian.werner@5minds.de>
2018-05-18 16:28:36 +0200

    Add descriptive comment to ProcessStartRequestPayload

--- src/data_models/process_model/process_start_request_payload.ts
+++ src/data_models/process_model/process_start_request_payload.ts
@@ -1,6 +1,6 @@
 // tslint:disable:variable-name
 export class ProcessStartRequestPayload {
   public correlation_id: string;
-  public callerId?: string;
+  public callerId?: string; // Usually a process instance ID
   public input_values: any;
 }
