cde6d036add498779cdcf9dd5689997b634ecdb7
ElRaptorus <christian.werner@5minds.de>
2018-05-18 16:41:37 +0200

    Implement requested changes

--- src/icorrelation_result.ts
+++ src/icorrelation_result.ts
@@ -1,10 +1,5 @@
 export interface ICorrelationResult {
   correlationId: string;
-  processInstanceResults: Array<IProcessInstanceResult>;
-}
-
-export interface IProcessInstanceResult {
-  processInstanceId: string;
   processModelKey: string;
   result: any;
 }
