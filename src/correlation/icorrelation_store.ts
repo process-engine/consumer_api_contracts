e69877d38b67b136e459c915b047af035969ddb4
ElRaptorus <christian.werner@5minds.de>
2018-05-18 11:50:33 +0200

    Add processModelKey as optional parameter to correlation store

--- src/correlation/icorrelation_store.ts
+++ src/correlation/icorrelation_store.ts
@@ -1,4 +1,4 @@
 export interface ICorrelationStore {
-  addProcessInstanceToCorrelation(correlationId: string, processInstanceId: string): void;
+  addProcessInstanceToCorrelation(correlationId: string, processInstanceId: string, processModelKey?: string): void;
   getProcessInstancesInCorrelation(correlationId: string): Array<string>;
 }
