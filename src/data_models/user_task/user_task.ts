4595fef119b12782b69f2b2027b1959149b74cf4
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-18 14:16:15 +0200

    add payload to usertask

--- src/data_models/user_task/user_task.ts
+++ src/data_models/user_task/user_task.ts
@@ -5,4 +5,5 @@ export class UserTask {
   public id: string;
   public processInstanceId: string;
   public data: UserTaskConfig;
+  public payload: any; // TODO (SM): fill this with token payload from suspension
 }
