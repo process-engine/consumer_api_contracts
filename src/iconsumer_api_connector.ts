5be459c1dc35cdab4c701b2618296ea4c6ba837f
ElRaptorus <christian.werner@5minds.de>
2018-06-18 09:56:36 +0200

    Rename consumer api connector to consumer api accessor

--- src/iconsumer_api_connector.ts
+++
@@ -1,6 +0,0 @@
-import {IConsumerApiService} from './iconsumer_api_service';
-
-// This interface wrappes the connector that the ConsumerApiClientService will use to connect to an internal or external process engine.
-// The signatures will be the same as with the IConsumerApiService, because the connector will have to perform the same type of requests,
-// regardless of which type of process engine is used.
-export interface IConsumerApiConnector extends IConsumerApiService {}
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * from './messagebus_api/index';
 
 export * from './consumer_context';
 export * from './consumer_request';
-export * from './iconsumer_api_connector';
+export * from './iconsumer_api_accessor';
 export * from './iconsumer_api_service';
 export * from './icorrelation_result';
 export * from './rest_settings';
