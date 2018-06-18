2eef150a7ef7fd570365d3d28383a7af16afc7f6
ElRaptorus <christian.werner@5minds.de>
2018-06-18 13:56:08 +0200

    Fix typos

--- src/iconsumer_api_accessor.ts
+++ src/iconsumer_api_accessor.ts
@@ -1,6 +1,6 @@
 import {IConsumerApiService} from './iconsumer_api_service';
 
-// This interface wrappes the accessor that the ConsumerApiClientService will use to connect to an internal or external process engine.
-// The signatures will be the same as with the IConsumerApiService, because the accessor will have to perform the same type of requests,
+// This interface wraps the accessor that the ConsumerApiClientService will use to connect to an internal or external process engine.
+// It is derived from IConsumerApiService, because the accessor will have to perform the same type of requests as the service,
 // regardless of which type of process engine is used.
 export interface IConsumerApiAccessor extends IConsumerApiService {}
