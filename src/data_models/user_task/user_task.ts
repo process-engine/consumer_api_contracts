75a11de34fc28d632a19d70ef8a565342113f7f9
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-25 15:48:32 +0200

    rename user task payload

--- src/data_models/user_task/user_task.ts
+++ src/data_models/user_task/user_task.ts
@@ -5,5 +5,5 @@ export class UserTask {
   public id: string;
   public processInstanceId: string;
   public data: UserTaskConfig;
-  public payload: any; // TODO (SM): fill this with token payload from suspension
+  public tokenPayload: any; // token payload the UserTask got suspended with
 }
