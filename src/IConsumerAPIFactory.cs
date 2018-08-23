88ee00b865b301ebf7cae319549d47d310c4bd0e
Thomas Konrad <thomas.konrad@5minds.de>
2018-08-23 11:36:32 +0200

    Initial commit

---
+++ src/IConsumerAPIFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace ProcessEngine.ConsumerAPI.Contracts
+{
+    /// <summary>
+    /// Interface for an factory for creating ConsumerAPI services.
+    /// </summary>
+    public interface IConsumerAPIFactory
+    {
+        /// <summary>
+        /// Factory method for creating a ConsumerAPIService.
+        /// </summary>
+        /// <returns>The ConsumerAPIService.</returns>
+        /// <param name="processEngineAPIBaseUrl">The base URL of the process engine.</param>
+        /// <param name="userAgent">The user agent to put in the HTTP header.</param>
+        IConsumerAPIService CreateConsumerAPIService(string processEngineAPIBaseUrl, string userAgent);
+    }
+}
---
+++ src/IConsumerAPIService.cs
@@ -0,0 +1,48 @@
+﻿namespace ProcessEngine.ConsumerAPI.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using ProcessEngine.Runtime.Contracts;
+
+    /// <summary>
+    /// Service for starting and managing BMPN processes.
+    /// </summary>
+    public interface IConsumerAPIService : IDisposable
+    {
+
+        /// <summary>
+        /// Starts an instance a given process definition. Process variables and correlation id may be supplied in the request payload.
+        /// </summary>
+        /// <returns>The process instance.</returns>
+        /// <param name="context">The authentication context of the consumer.</param>
+        /// <param name="processModelId">The key of the process definition (the latest version thereof) to be retrieved.</param>
+        /// <param name="startEventKey">The key of a specific start event to start the process with.</param>
+        /// <param name="payload">The payload to supply process variables and an optional correlation id.</param>
+        /// <param name="callbackType"><see cref="StartCallbackType">Callback type</see></param>
+        /// <param name="endEventKey">The key of the end event when startCallbackType == StartCallbackType.CallbackOnEndEventReached.</param>
+        /// <typeparam name="TProcessStartRequestPayload">Parameter with initial data for the process.</typeparam>
+        /// <typeparam name="TProcessStartResponsePayload">Parameter that should return a unique process Id and correlation Id.</typeparam>
+        Task<TProcessStartResponsePayload> StartProcessInstance<TProcessStartRequestPayload, TProcessStartResponsePayload>(
+            ConsumerContext context,
+            string processModelId,
+            string startEventKey,
+            TProcessStartRequestPayload payload,
+            StartCallbackType callbackType = StartCallbackType.CallbackOnProcessInstanceCreated,
+            string endEventKey = "")
+            where TProcessStartResponsePayload : new();
+
+        /// <summary>
+        /// Gets the process result for a given correlation.
+        /// </summary>
+        /// <returns>The process result for the correlation.</returns>
+        /// <param name="context">The authentication context of the consumer.</param>
+        /// <param name="correlationId">The correlation id generated at the start of the process instance.</param>
+        /// <param name="processModelId">The key of the process definition (the latest version thereof) to be retrieved.</param>
+        /// <typeparam name="TPayload">Parameter holding the result data for a correlation.</typeparam>
+        Task<IEnumerable<CorrelationResult<TPayload>>> GetProcessResultForCorrelation<TPayload>(ConsumerContext context, string correlationId, string processModelId)
+            where TPayload : new();
+
+    }
+}
