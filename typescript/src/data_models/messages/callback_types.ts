48d5d39ec39cb5cf9dfc441068b608fa6cbdf4a3
robpal94 <robin.palkovits@5minds.de>
2018-11-23 12:23:24 +0100

    :ok_hand: unify code layout

--- typescript/src/data_models/messages/callback_types.ts
+++ typescript/src/data_models/messages/callback_types.ts
@@ -9,12 +9,12 @@ import {
 } from './system_events';
 
 export type OnUserTaskWaitingCallback = (userTaskWaiting: UserTaskReachedMessage) => void | Promise<void>;
-export type OnUserTaskFinishedCallback = (userTaskFinished: UserTaskFinishedMessage) => void|Promise<void>;
+export type OnUserTaskFinishedCallback = (userTaskFinished: UserTaskFinishedMessage) => void | Promise<void>;
 
 export type OnManualTaskWaitingCallback = (manualTaskWaiting: ManualTaskReachedMessage) => void | Promise<void>;
 export type OnManualTaskFinishedCallback = (manualTaskFinished: ManualTaskFinishedMessage) => void | Promise<void>;
 
-export type OnProcessStartedCallback = (processStarted: ProcessStartedMessage) => void|Promise<void>;
+export type OnProcessStartedCallback = (processStarted: ProcessStartedMessage) => void | Promise<void>;
 
-export type OnProcessEndedCallback = (processEnded: ProcessEndedMessage) => void|Promise<void>;
-export type OnProcessTerminatedCallback = (processTerminated: ProcessTerminatedMessage) => void|Promise<void>;
+export type OnProcessEndedCallback = (processEnded: ProcessEndedMessage) => void | Promise<void>;
+export type OnProcessTerminatedCallback = (processTerminated: ProcessTerminatedMessage) => void | Promise<void>;
