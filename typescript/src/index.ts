1b420b5e9cb49589fe0485bbe080bd38b8ab781e
ElRaptorus <christian.werner@5minds.de>
2019-01-08 13:47:55 +0100

    :sparkles: Add interface for http controller

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,7 @@
 export * from './data_models/index';
 
 export * from './iconsumer_api_accessor';
+export * from './iconsumer_api_http_controller';
 export * from './iconsumer_api';
 export * from './rest_settings';
 export * from './socket_settings';
