719910e383fbae474ada882cb7d06762e7514351
ElRaptorus <christian.werner@5minds.de>
2019-01-08 13:16:24 +0100

    :sparkles: Add subscriptions for identity based notifications

--- typescript/src/socket_settings.ts
+++ typescript/src/socket_settings.ts
@@ -1,11 +1,14 @@
 // tslint:disable:typedef
 const pathParams = {
   processModelId: 'process_model_id',
+  userId: 'user_id',
 };
 
 const paths = {
   userTaskWaiting: 'user_task_waiting',
+  userTaskForIdentityWaiting: `user_task_waiting/user_id/${pathParams.userId}`,
   userTaskFinished: 'user_task_finished',
+  userTaskForIdentityFinished: `user_task_finished/user_id/${pathParams.userId}`,
   manualTaskWaiting: 'manual_task_waiting',
   manualTaskFinished: 'manual_task_finished',
   processEnded: 'process_ended',
