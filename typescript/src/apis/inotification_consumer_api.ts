98bcc650db364651e7b444d556e5ceb8ea896738
ElRaptorus <christian.werner@5minds.de>
2019-01-21 12:47:58 +0100

    :sparkles: Add IIdentity to removeSubscription use case

--- typescript/src/apis/inotification_consumer_api.ts
+++ typescript/src/apis/inotification_consumer_api.ts
@@ -1,4 +1,5 @@
 import {Subscription} from '@essential-projects/event_aggregator_contracts';
+import {IIdentity} from '@essential-projects/iam_contracts';
 
 /**
  * The INotificationConsumerApi is to manage subscriptions for async notifications.
@@ -9,7 +10,8 @@ export interface INotificationConsumerApi {
    * Removes the given notification subscription.
    *
    * @async
+   * @param identity     The requesting users identity.
    * @param subscription The subscription to remove.
    */
-  removeSubscription(subscription: Subscription): Promise<void>;
+  removeSubscription(identity: IIdentity, subscription: Subscription): Promise<void>;
 }
