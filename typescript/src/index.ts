d6cfed5650e947b29643715d85c6c96d54c454b4
ElRaptorus <christian.werner@5minds.de>
2019-01-22 11:36:32 +0100

    :sparkles: Provide an interface for a Socket.IO client

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -5,5 +5,6 @@ export * from './messages/index';
 export * from './iconsumer_api_accessor';
 export * from './iconsumer_api_http_controller';
 export * from './iconsumer_api';
+export * from './iconsumer_socket_io_accessor';
 export * from './rest_settings';
 export * from './socket_settings';
