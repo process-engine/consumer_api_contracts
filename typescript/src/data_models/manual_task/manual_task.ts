192296763b41b9289004b4bc536081c38e04ad0b
ElRaptorus <christian.werner@5minds.de>
2019-03-07 11:11:42 +0100

    :book: Import doc changes from Management API

--- typescript/src/data_models/manual_task/manual_task.ts
+++ typescript/src/data_models/manual_task/manual_task.ts
@@ -1,5 +1,5 @@
 /**
- * Describes a MaualTask that belongs to an active Correlation.
+ * Describes a suspended ManualTask that is waiting to be finished.
  */
 export class ManualTask {
   /**
@@ -14,17 +14,8 @@ export class ManualTask {
    * The instance ID of the ManualTask.
    */
   public flowNodeInstanceId?: string;
-  /**
-   * The ID of the Correlation that the ManualTask belongs to.
-   */
   public correlationId: string;
-  /**
-   * The ID of the ProcessModel that the ManualTask belongs to.
-   */
   public processModelId: string;
-  /**
-   * The ID of the ProcessInstance that the ManualTask belongs to.
-   */
   public processInstanceId?: string;
   /**
    * The token payload the ManualTask got suspended with.
