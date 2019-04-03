4a4cee03a450dba3cb4d82e3a11d481a0a68cc22
ElRaptorus <christian.werner@5minds.de>
2019-04-03 11:53:02 +0200

    :recycle: Convert UserTaskFormFieldType to enum

--- dotnet/src/data_models/user_task/UserTaskFormFieldType.cs
+++ dotnet/src/data_models/user_task/UserTaskFormFieldType.cs
@@ -3,36 +3,36 @@ namespace ProcessEngine.ConsumerAPI.Contracts.DataModel
     /// <summary>
     /// Determines the type of a UserTask's form field.
     /// </summary>
-    public static class UserTaskFormFieldType
+    public enum UserTaskFormFieldType
     {
         /// <summary>
         /// The form field is a boolean.
         /// </summary>
-        public static readonly string Boolean = "boolean";
+        Boolean,
 
         /// <summary>
         /// The form field is a date.
         /// </summary>
-        public static readonly string Date = "date";
+        Date,
 
         /// <summary>
         /// The form field is an enumeration.
         /// </summary>
-        public static readonly string Enum = "enum";
+        Enum,
 
         /// <summary>
         /// The form field is long-type numerical.
         /// </summary>
-        public static readonly string Long = "long";
+        Long,
 
         /// <summary>
         /// The form field is a number.
         /// </summary>
-        public static readonly string Number = "number";
+        Number,
 
         /// <summary>
         /// The form field is a string.
         /// </summary>
-        public static readonly string String = "string";
+        String
     }
 }
