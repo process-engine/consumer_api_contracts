b46abbff7f793a8074f045de70a5b6b2c7cdaea0
Steffen <Steffen.knaup@5minds.de>
2019-06-03 11:15:22 +0200

    :bug: Add Missing IntermediateCatchEvent Event

--- typescript/src/apis/inotification_consumer_api.ts
+++ typescript/src/apis/inotification_consumer_api.ts
@@ -97,9 +97,9 @@ export interface INotificationConsumerApi {
    * @async
    * @param   identity           The requesting users identity.
    * @param   callback           The callback that will be executed when a
-   *                             new IntermediateEvent is triggered.
+   *                             new IntermediateThrowEvent is triggered.
    *                             The message passed to the callback contains
-   *                             further information about the IntermediateEvent.
+   *                             further information about the IntermediateThrowEvent.
    * @param   subscribeOnce      Optional: If set to true, the subscription will
    *                             be automatically disposed, after the notification
    *                             was received once.
@@ -117,14 +117,39 @@ export interface INotificationConsumerApi {
   ): Promise<Subscription>;
 
   /**
-   * Executes the provided callback when a IntermediateEvent is finished.
+   * Executes the provided callback when a IntermediateCatchEvent is reached.
    *
    * @async
    * @param   identity           The requesting users identity.
    * @param   callback           The callback that will be executed when an
-   *                             IntermediateEvent is finished.
+   *                             IntermediateCatchEvent is reached.
    *                             The message passed to the callback contains
-   *                             further information about the IntermediateEvent.
+   *                             further information about the IntermediateCatchEvent.
+   * @param   subscribeOnce      Optional: If set to true, the subscription will
+   *                             be automatically disposed, after the notification
+   *                             was received once.
+   * @returns                    The subscription created by the EventAggregator.
+   *
+   * @throws {UnauthorizedError} If the given identity does not contain a
+   *                             valid auth token.
+   * @throws {ForbiddenError}    If the user is not allowed to create
+   *                             event subscriptions.
+   */
+  onIntermediateCatchEventReached(
+    identity: IIdentity,
+    callback: Messages.CallbackTypes.OnIntermediateCatchEventReachedCallback,
+    subscribeOnce?: boolean,
+  ): Promise<Subscription>;
+
+  /**
+   * Executes the provided callback when a IntermediateCatchEvent is finished.
+   *
+   * @async
+   * @param   identity           The requesting users identity.
+   * @param   callback           The callback that will be executed when an
+   *                             IntermediateCatchEvent is finished.
+   *                             The message passed to the callback contains
+   *                             further information about the IntermediateCatchEvent.
    * @param   subscribeOnce      Optional: If set to true, the subscription will
    *                             be automatically disposed, after the notification
    *                             was received once.
--- typescript/src/messages/system_events/intermediate_throw_event_triggered.ts
+++ typescript/src/messages/system_events/intermediate_throw_event_triggered.ts
@@ -1,6 +1,6 @@
 import {BaseEventMessage} from '../base_event_message';
 
 /**
- * Represents the message which is send, when an IntermediateEvent has been triggered.
+ * Represents the message which is send, when an IntermediateThrowEvent has been triggered.
  */
 export class IntermediateThrowEventTriggeredMessage extends BaseEventMessage {}
