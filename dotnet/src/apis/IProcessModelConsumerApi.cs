7be14a64dc7aeefe43b5c9e4e4f25119ea84a2ce
ElRaptorus <christian.werner@5minds.de>
2019-07-23 13:48:05 +0200

    :sparkles: Sync .NET ProcessModelAPI with .TS ProcessModelApi

--- dotnet/src/apis/IProcessModelConsumerApi.cs
+++ dotnet/src/apis/IProcessModelConsumerApi.cs
@@ -2,16 +2,55 @@ namespace ProcessEngine.ConsumerAPI.Contracts.APIs
 {
     using System.Collections.Generic;
     using System.Threading.Tasks;
-    using System;
 
     using EssentialProjects.IAM.Contracts;
     using ProcessEngine.ConsumerAPI.Contracts.DataModel;
+    using ProcessEngine.ConsumerAPI.Contracts.Exceptions;
 
     /// <summary>
     /// The IProcessModelConsumerApi is used to retreive ProcessModels and start ProcessInstances.
     /// </summary>
     public interface IProcessModelConsumerApi
     {
+        /// <summary>
+        /// Retrieves a list of all ProcessModels that the requesting user is authorized to see.
+        /// </summary>
+        /// <param name="identity">The requesting users identity.</param>
+        /// <returns>A list of accessible ProcessModels.</returns>
+        /// <exception cref="System.UnauthorizedAccessException">Thrown when the identity has insufficient rights to perform the operation.</exception>
+        /// <exception cref="ProcessNotFoundException"> Thrown when the ProcessModel could not be found.</exception>
+        Task<ProcessModelList> GetProcessModels(IIdentity identity);
+
+        /// <summary>
+        /// Retrieves a ProcessModel by its ID.
+        /// </summary>
+        /// <param name="identity">The requesting users identity.</param>
+        /// <param name="processModelId">The ID of the ProcessModel to retrieve.</param>
+        /// <returns>The retrieved ProcessModel.</returns>
+        /// <exception cref="System.UnauthorizedAccessException">Thrown when the identity has insufficient rights to perform the operation.</exception>
+        /// <exception cref="ProcessNotFoundException"> Thrown when the ProcessModel could not be found.</exception>
+        Task<ProcessModel> GetProcessModelById(IIdentity identity, string processModelId);
+
+        /// <summary>
+        /// Retrieves a ProcessModel by a ProcessInstanceID.
+        /// </summary>
+        /// <param name="identity">The requesting users identity.</param>
+        /// <param name="processInstanceId">The ID of the ProcessInstance for which to retrieve the ProcessModel.</param>
+        /// <returns>The retrieved ProcessModel.</returns>
+        /// <exception cref="System.UnauthorizedAccessException">Thrown when the identity has insufficient rights to perform the operation.</exception>
+        /// <exception cref="ProcessNotFoundException"> Thrown when the ProcessModel could not be found.</exception>
+        Task<ProcessModel> GetProcessModelByProcessInstanceId(IIdentity identity, string processInstanceId);
+
+        /// <summary>
+        /// Gets all active ProcessInstances belonging to the given identity.
+        /// </summary>
+        /// <param name="identity">The identity for which to get the ProcessInstances.</param>
+        /// <returns>The list of ProcessInstances.</returns>
+        /// <exception cref="System.UnauthorizedAccessException">Thrown when the identity has insufficient rights to perform the operation.</exception>
+        /// <exception cref="ProcessNotFoundException"> Thrown when the ProcessModel could not be found.</exception>
+        Task<IEnumerable<ProcessInstance>> GetProcessInstancesByIdentity(IIdentity identity);
+
+
         /// <summary>
         /// Starts an instance for a given ProcessDefinition. Process variables and correlation id may be supplied in the request payload.
         /// </summary>
