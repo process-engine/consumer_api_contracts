6560fd186488b76974ac736e321f8cf82623ef0e
ElRaptorus <christian.werner@5minds.de>
2019-07-31 09:45:12 +0200

    :recycle: Split HttpControllers into UseCase specific interfaces

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,10 +1,10 @@
 export * from './apis/index';
 export * from './data_models/index';
+export * from './http_controller/index';
 export * from './messages/index';
 
 export * from './iconsumer_api_accessor';
 export * from './iconsumer_api_client';
-export * from './iconsumer_api_http_controller';
 export * from './iconsumer_socket_io_accessor';
 export * from './rest_settings';
 export * from './socket_settings';
