7c59f054b9097d3e96130da2d6a55f819158ccac
ElRaptorus <christian.werner@5minds.de>
2019-08-07 13:04:18 +0200

    :sparkles: Add .ts REST settings for ExternalTaskApi

--- typescript/src/rest_settings.ts
+++ typescript/src/rest_settings.ts
@@ -1,30 +1,18 @@
 const params = {
-  processInstanceId: ':process_instance_id',
-  processModelId: ':process_model_id',
   correlationId: ':correlation_id',
-  startEventId: ':start_event_id',
+  emptyActivityInstanceId: ':empty_activity_instance_id',
   endEventId: ':end_event_id',
   eventId: ':event_id',
   eventName: ':event_name',
-  emptyActivityInstanceId: ':empty_activity_instance_id',
-  userTaskInstanceId: ':user_task_instance_id',
+  externalTaskId: ':external_task_id',
   manualTaskInstanceId: ':manual_task_instance_id',
+  processInstanceId: ':process_instance_id',
+  processModelId: ':process_model_id',
+  startEventId: ':start_event_id',
+  userTaskInstanceId: ':user_task_instance_id',
 };
 
 const paths = {
-  // ProcessModels
-  processModels: '/process_models',
-  processModelById: `/process_models/${params.processModelId}`,
-  processModelByProcessInstanceId: `/process_instance/${params.processInstanceId}/process_model/`,
-  startProcessInstance: `/process_models/${params.processModelId}/start`,
-  getOwnProcessInstances: '/process_instances/own',
-  getProcessResultForCorrelation: `/correlations/${params.correlationId}/process_models/${params.processModelId}/results`,
-  // Events
-  processModelEvents: `/process_models/${params.processModelId}/events`,
-  correlationEvents: `/correlations/${params.correlationId}/events`,
-  processModelCorrelationEvents: `/process_models/${params.processModelId}/correlations/${params.correlationId}/events`,
-  triggerMessageEvent: `/message/${params.eventName}/trigger`,
-  triggerSignalEvent: `/signal/${params.eventName}/trigger`,
   // EmptyActivities
   processModelEmptyActivities: `/process_models/${params.processModelId}/empty_activities`,
   processInstanceEmptyActivities: `/process_instances/${params.processInstanceId}/empty_activities`,
@@ -33,13 +21,18 @@ const paths = {
   getOwnEmptyActivities: '/empty_activities/own',
   finishEmptyActivity:
     `/processes/${params.processInstanceId}/correlations/${params.correlationId}/empty_activities/${params.emptyActivityInstanceId}/finish`,
-  // UserTasks
-  processModelUserTasks: `/process_models/${params.processModelId}/user_tasks`,
-  processInstanceUserTasks: `/process_instances/${params.processInstanceId}/user_tasks`,
-  correlationUserTasks: `/correlations/${params.correlationId}/user_tasks`,
-  processModelCorrelationUserTasks: `/process_models/${params.processModelId}/correlations/${params.correlationId}/user_tasks`,
-  getOwnUserTasks: '/user_tasks/own',
-  finishUserTask: `/processes/${params.processInstanceId}/correlations/${params.correlationId}/usertasks/${params.userTaskInstanceId}/finish`,
+  // Events
+  processModelEvents: `/process_models/${params.processModelId}/events`,
+  correlationEvents: `/correlations/${params.correlationId}/events`,
+  processModelCorrelationEvents: `/process_models/${params.processModelId}/correlations/${params.correlationId}/events`,
+  triggerMessageEvent: `/message/${params.eventName}/trigger`,
+  triggerSignalEvent: `/signal/${params.eventName}/trigger`,
+  // ExternalTasks
+  fetchAndLockExternalTasks: '/fetch_and_lock',
+  extendExternalTaskLock: `/task/${params.externalTaskId}/extend_lock`,
+  finishExternalTaskWithBpmnError: `/task/${params.externalTaskId}/handle_bpmn_error`,
+  finishExternalTaskWithServiceError: `/task/${params.externalTaskId}/handle_service_error`,
+  finishExternalTask: `/task/${params.externalTaskId}/finish`,
   // ManualTask
   processModelManualTasks: `/process_models/${params.processModelId}/manual_tasks`,
   processInstanceManualTasks: `/process_instances/${params.processInstanceId}/manual_tasks`,
@@ -48,6 +41,20 @@ const paths = {
   getOwnManualTasks: '/manual_tasks/own',
   finishManualTask: `/processes/${params.processInstanceId}/correlations/${params.correlationId}/` +
     `manual_tasks/${params.manualTaskInstanceId}/finish`,
+  // ProcessModels
+  processModels: '/process_models',
+  processModelById: `/process_models/${params.processModelId}`,
+  processModelByProcessInstanceId: `/process_instance/${params.processInstanceId}/process_model/`,
+  startProcessInstance: `/process_models/${params.processModelId}/start`,
+  getOwnProcessInstances: '/process_instances/own',
+  getProcessResultForCorrelation: `/correlations/${params.correlationId}/process_models/${params.processModelId}/results`,
+  // UserTasks
+  processModelUserTasks: `/process_models/${params.processModelId}/user_tasks`,
+  processInstanceUserTasks: `/process_instances/${params.processInstanceId}/user_tasks`,
+  correlationUserTasks: `/correlations/${params.correlationId}/user_tasks`,
+  processModelCorrelationUserTasks: `/process_models/${params.processModelId}/correlations/${params.correlationId}/user_tasks`,
+  getOwnUserTasks: '/user_tasks/own',
+  finishUserTask: `/processes/${params.processInstanceId}/correlations/${params.correlationId}/usertasks/${params.userTaskInstanceId}/finish`,
 };
 
 export const restSettings = {
