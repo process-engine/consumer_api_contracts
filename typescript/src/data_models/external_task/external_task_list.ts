fbf6812cecff1516fa94e673bc3214c882290fa6
Alex😎 <alexander.kasten@5minds.de>
2019-09-17 13:52:49 +0200

    :fire: Remove total count for external tasks

--- typescript/src/data_models/external_task/external_task_list.ts
+++
@@ -1,9 +0,0 @@
-import {ExternalTask} from './external_task';
-
-export class ExternalTaskList<TPayload> {
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public externalTasks: Array<ExternalTask<TPayload>>;
-  public totalCount: number;
-
-}
--- typescript/src/data_models/external_task/index.ts
+++ typescript/src/data_models/external_task/index.ts
@@ -3,4 +3,3 @@ export * from './results/index';
 
 export * from './external_task';
 export * from './external_task_state';
-export * from './external_task_list';
