2154c2cc0cd44f4eee9d3af53cf5a8cdf397ec80
ElRaptorus <christian.werner@5minds.de>
2020-01-22 11:13:56 +0100

    :no_entry_sign: Impose type restriction on .ts ExternalTaskSuccessResult

--- typescript/src/data_models/external_task/results/external_task_success_result.ts
+++ typescript/src/data_models/external_task/results/external_task_success_result.ts
@@ -2,12 +2,13 @@ import {ExternalTaskResultBase} from './external_task_result_base';
 
 /**
  * Contains the result set for an ExternalTask that finished successfully.
+ * The result must be a valid JSON or any other valid object type.
  */
-export class ExternalTaskSuccessResult extends ExternalTaskResultBase {
+export class ExternalTaskSuccessResult<TResult extends object> extends ExternalTaskResultBase {
 
-  public readonly result: any;
+  public readonly result: TResult;
 
-  constructor(externalTaskId: string, result: any) {
+  constructor(externalTaskId: string, result: TResult) {
     super(externalTaskId);
     this.result = result;
   }
