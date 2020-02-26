cd519cd1b96d5f48d9b7d90f2f712582876f6894
ElRaptorus <christian.werner@5minds.de>
2020-02-26 10:44:07 +0100

    :sparkles: Add interface for IApplicationInfo service

---
+++ typescript/src/apis/iapplication_info_consumer_api.ts
@@ -0,0 +1,15 @@
+import {ApplicationInfo} from '../data_models/application_info';
+
+/**
+ * The IApplicationInfoConsumerApi is used to retrieve basic information about the running application..
+ */
+export interface IApplicationInfoConsumerApi {
+
+  /**
+   * Gets the package name and version of the running application.
+   *
+   * @async
+   * @returns Some Basic information about the running application.
+   */
+  getApplicationInfo(): Promise<ApplicationInfo>;
+}
