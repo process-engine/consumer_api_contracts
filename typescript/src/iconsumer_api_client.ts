8b8354458a3b8e81de2d040c4e5b98898ae0ddf7
ElRaptorus <christian.werner@5minds.de>
2020-02-26 12:02:02 +0100

    :sparkles: Use ApplicationInfo routes in accessor

--- typescript/src/iconsumer_api_client.ts
+++ typescript/src/iconsumer_api_client.ts
@@ -1,11 +1,11 @@
 import {APIs} from './apis/index';
-import {ApplicationInfo} from './data_models/application_info';
 
 /**
  * The client provides endpoints for all UseCases the ConsumerAPI employs.
  */
 export interface IConsumerApiClient
-  extends APIs.IEmptyActivityConsumerApi,
+  extends APIs.IApplicationInfoConsumerApi,
+  APIs.IEmptyActivityConsumerApi,
   APIs.IEventConsumerApi,
   APIs.IExternalTaskConsumerApi,
   APIs.IManualTaskConsumerApi,
@@ -13,10 +13,3 @@ export interface IConsumerApiClient
   APIs.IProcessModelConsumerApi,
   APIs.IUserTaskConsumerApi,
   APIs.IFlowNodeInstanceConsumerApi {}
-
-/**
- * The client interface for getting information about the ProcessEngineRuntime.
- */
-export interface IDomainInfoclient {
-  getApplicationInfo(): Promise<ApplicationInfo>;
-}
